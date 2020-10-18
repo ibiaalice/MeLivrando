097dafbfbde293075a2aa60b3b114713cd175869
Beatriz Alice <beatrizalice00@gmail.com>
2020-10-17 23:05:14 -0300

    adicionando correção

--- frontend/melivrando/src/pages/Register/Register.js
+++ frontend/melivrando/src/pages/Register/Register.js
@@ -8,18 +8,14 @@ function Register(props){
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
-    async function  handleRegister(){
-        console.log('entrando neesssa porra');
-       await api.post('register/', {
-            name,
-            email,
-            password,
-        }).then(() => {
-            alert('Cadastro realizado com sucesso!')
-        }).catch(() => {
-           
-            alert('Erro no cadastro.');
-        });
+    async function  handleRegister(e){
+        e.preventDefault();
+        const data = new FormData();
+        data.append('name', name);
+        data.append('email', email);
+        data.append('password', password);
+
+        await api.post('register',data);
 
     }
     return(
@@ -38,19 +34,19 @@ function Register(props){
                         name='name'
                         placeholder='Digite o nome'
                         value={name}
-                        onChange={(e) => {setName(e.target.value)}} 
+                        onChange={(e) => setName(e.target.value)} 
                     />
                 <input type="email"
                     id='email'
                     name='email'
                     placeholder='Digite o email de login'
                     value={email}
-                    onChange={(e) => {setEmail(e.target.value)}} 
+                    onChange={(e) => setEmail(e.target.value)} 
                 />
                 <input type='password'
                     placeholder= 'Digite sua senha'
                     value={password}
-                    onChange={(e) => {setPassword(e.target.value)}}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
                  <button type="submit"> Inscrever-se </button>
             </form>
--- frontend/melivrando/src/services/api.js
+++ frontend/melivrando/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+
+
 const api = axios.create({
-    baseURL: 'http://localhost:8080/',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/'
 });
 
 export default api;
